0126436b60f279d76f94fa8f01bb92d0c2b9ff4a
masanori <>
2017-07-29 14:06:18 +0900

    fixed migration2

--- db/migrate/20170710071943_remove_avatar_from_schools.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAvatarFromSchools < ActiveRecord::Migration
-  def change
-    remove_column :schools, :avatar_file_name, :string
-    remove_column :schools, :avatar_content_type, :string
-    remove_column :schools, :avatar_file_size, :integer
-    remove_column :schools, :avatar_updated_at, :datetime
-  end
-end
--- db/migrate/20170721031417_remove_license_2_from_schools.rb
+++
@@ -1,5 +0,0 @@
-class RemoveLicense2FromSchools < ActiveRecord::Migration
-  def change
-    remove_column :schools, :license_2, :string
-  end
-end
