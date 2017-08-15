3766db239c7dba18632464c62ef84007d1cbc534
masanori <satomasanori@MacBook-Air.local>
2017-08-15 23:40:01 +0900

    css up10

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -7,11 +7,11 @@ def post_email(name,email,text)
     mail to: "bestdroneschool@gmail.com", subject: "ベストドローンスクールに問い合わせがありました。"
 end
 
-def post_mail(nickname,rate,sex,position,good,bad)
+def post_mail(nickname,rate,sex,Position,good,bad)
     @nickname = nickname
     @rate = rate
     @sex = sex
-    @position = position
+    @position = Position
     @good = good
     @bad = bad
     mail to: "bestdroneschool@gmail.com", subject: "口コミ投稿がありました。"
