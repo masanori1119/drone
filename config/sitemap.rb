27d0a6650f33ab644aedb4da1465d4da50e206bf
masanori <ageagegogo1119@yahoo.co.jp>
2017-12-21 16:12:58 +0900

    “SEO対策修正②”

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -12,12 +12,3 @@ SitemapGenerator::Sitemap.create do
   add root_path, :priority => 0.7, :changefreq => 'daily'
 
 end
-
-:storage => :s3,
-  :s3_region => 'ap-northeast-1',
-  :s3_endpoint => '.ap-northeast-1.amazonaws.com',
-  :bucket => 'upload-test1234',
-  :enable_starttls_auto => true,
-  :s3_credentials => {
-  :access_key_id => ENV["ACCESS_KEY_ID"],
-  :secret_access_key => ENV["SECRET_ACCESS_KEY"],
